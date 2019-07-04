9e580c239375e15928642b59a809145aedaac8c8
lix3972 <35994518+lix3972@users.noreply.github.com>
2019-07-04 12:26:16 +0800

    Create install.sh

---
+++ software/stardict/install.sh
@@ -0,0 +1,11 @@
+sudo apt-get install stardict
+
+sudo tar -xjvf stardict-cedict-gb-2.4.2.tar.bz2 -C /usr/share/stardict/dic
+sudo tar -xjvf stardict-langdao-ce-gb-2.4.2.tar.bz2 -C /usr/share/stardict/dic
+sudo tar -xjvf stardict-langdao-ec-gb-2.4.2.tar.bz2 -C /usr/share/stardict/dic
+sudo tar -xjvf stardict-oxford-gb-2.4.2.tar.bz2 -C /usr/share/stardict/dic
+sudo tar -xjvf stardict-powerword2011_1_900-2.4.2.tar.bz2 -C /usr/share/stardict/dic
+sudo tar -xjvf stardict-powerword2011_1_901-2.4.2.tar.bz2 -C /usr/share/stardict/dic
+sudo tar -xjvf stardict-xdict-ce-gb-2.4.2.tar.bz2 -C /usr/share/stardict/dic
+sudo tar -xjvf stardict-xdict-ce-gb_fix-2.4.2.tar.bz2 -C /usr/share/stardict/dic
+sudo tar -xjvf stardict-xdict-ec-gb-2.4.2.tar.bz2 -C /usr/share/stardict/dic
